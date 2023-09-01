c32ab10302699dee3b414b3aa677ebc5e5044fbe
Alessio Marino <alessio.marino@mrjaffesclass.com>
2023-09-01 11:24:27 -0700

    finished datatypes

--- HelloWorld1/HelloWorld.java
+++ HelloWorld1/HelloWorld.java
@@ -13,7 +13,8 @@ public class HelloWorld
         System.out.println(i);
         System.out.println(f);
         System.out.println(flag);
-        
+    }
+    public static void mainstring (String[] args){
         String string1 = "I am a string";
         String string2 = string1;
         String string3 = "I am also a string";
@@ -23,5 +24,23 @@ public class HelloWorld
         System.out.println(string2);
         System.out.println(string3);
         System.out.println(stringsEqual);
+    }
+    public static void mainarray (String[] args){
+        String[] arraystring = {"Alpha", "Bravo", "Charlie", "Delta", "Echo"};
+        for (int i = 0; i < arraystring.length; i++) {
+            System.out.println(arraystring[i]);
+        }
+        System.out.println("");
+        for (String value : arraystring) {
+            System.out.print(value);
+        }
+        System.out.println("");
+        System.out.println("");
+        for (int i = 0; i < arraystring.length; i++) {
+            System.out.print(arraystring[i]);
+            if (i != arraystring.length-1) {
+                System.out.print("-");
+            }
+        }
     }
 }
